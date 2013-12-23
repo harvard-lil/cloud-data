7353e3c97ad43569f96bcf04ff8ff7a4bc88aefb
Matthew Phillips <labratmatt@Matthews-MacBook-Pro-3.local>
2013-12-22 23:47:04 -0500

    top 2000 data file added

--- cloud.py
+++ cloud.py
@@ -1,5 +1,5 @@
 import dns.resolver
-import time, json
+import time, json, re, csv
 import requests
 
 # loop through lines in top_2000_alexa_sites.txt
@@ -12,39 +12,60 @@ import requests
 # Alexa rank, name, address, org name, org handle, cloud flag
 # 1, google.com, 1.1.1.1, google inc, goog, 0
 
+
+# Some common cloud hosts
+cloud_providers = ['amazon', 'rackspace', 'softlayer', 'microsoft', 'google']
+
+
+rows = []
+
 f = open('top_2000_alexa_sites.txt', 'r')
 
 rank = 1
 
 for line in f:
-    print rank
+    row = []
+    row.append(rank)
     name = line.rstrip()
     
     try:
         answers = dns.resolver.query(name, 'A')
         ip_address = answers[0].address
 
-        print name, ip_address
+        row.append(name)
+        row.append(ip_address)
+
         # get org from IP from ARIN API
         url = 'http://whois.arin.net/rest/ip/' + ip_address
         headers = {'Accept': 'application/json'}
         r = requests.get(url, headers=headers)
         jr = json.loads(r.text)
+        
+        cloud_based = 0
 
         if 'net' in jr and 'orgRef' in jr['net'] and '@name' in jr['net']['orgRef']:
-            print jr['net']['orgRef']['@name']
-            print jr['net']['orgRef']['@handle']
-        else:
-            print "no org found"
+            org_name = jr['net']['orgRef']['@name']
+            row.append(org_name)
+            row.append(jr['net']['orgRef']['@handle'])
+
+            for cloud_provider in cloud_providers:
+                if bool(re.search(cloud_provider, org_name.lower())):
+
+                    cloud_based = cloud_providers.index(cloud_provider) + 1
+                    break
             
+        row.append(cloud_based)
     except:
         pass
 
-    print ""
-
-    if rank == 5:
-        break    
+    rows.append(row)
+    
+    if not rank % 100:
+        print "indexed %s" % rank   
     
     rank += 1
     time.sleep(1)
     
+with open('top_2000.csv', 'w') as fp:
+    a = csv.writer(fp, delimiter=',')
+    a.writerows(rows)
