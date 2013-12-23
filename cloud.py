961e1a48201ec1055cbbeb514f92c811cf4339ff
Matt Phillips <mphillips@law.harvard.edu>
2013-12-23 16:10:13 -0500

    added a small html table building method

--- cloud.py
+++ cloud.py
@@ -13,59 +13,97 @@ import requests
 # 1, google.com, 1.1.1.1, google inc, goog, 0
 
 
-# Some common cloud hosts
-cloud_providers = ['amazon', 'rackspace', 'softlayer', 'microsoft', 'google']
+
+def get_arin_data():
+
+    # Some common cloud hosts
+    cloud_providers = ['amazon', 'rackspace', 'softlayer', 'microsoft', 'google']
 
 
-rows = []
+    rows = []
 
-f = open('top_2000_alexa_sites.txt', 'r')
+    f = open('top_2000_alexa_sites.txt', 'r')
 
-rank = 1
+    rank = 1
 
-for line in f:
-    row = []
-    row.append(rank)
-    name = line.rstrip()
+    for line in f:
+        row = []
+        row.append(rank)
+        name = line.rstrip()
     
-    try:
-        answers = dns.resolver.query(name, 'A')
-        ip_address = answers[0].address
+        try:
+            answers = dns.resolver.query(name, 'A')
+            ip_address = answers[0].address
+
+            # get org from IP from ARIN API
+            url = 'http://whois.arin.net/rest/ip/' + ip_address
+            headers = {'Accept': 'application/json'}
+            r = requests.get(url, headers=headers)
+            jr = json.loads(r.text)
+        
+            org_name = ""
+            org_handle = ""
+            cloud_based = 0
+
+            if 'net' in jr and 'orgRef' in jr['net'] and '@name' in jr['net']['orgRef']:
+                org_name = jr['net']['orgRef']['@name']
+                org_handle = jr['net']['orgRef']['@handle']
+
+
+                for cloud_provider in cloud_providers:
+                    if bool(re.search(cloud_provider, org_name.lower())):
+
+                        cloud_based = cloud_providers.index(cloud_provider) + 1
+                        break
+        except:
+            pass
 
         row.append(name)
         row.append(ip_address)
-
-        # get org from IP from ARIN API
-        url = 'http://whois.arin.net/rest/ip/' + ip_address
-        headers = {'Accept': 'application/json'}
-        r = requests.get(url, headers=headers)
-        jr = json.loads(r.text)
-        
-        cloud_based = 0
-
-        if 'net' in jr and 'orgRef' in jr['net'] and '@name' in jr['net']['orgRef']:
-            org_name = jr['net']['orgRef']['@name']
-            row.append(org_name)
-            row.append(jr['net']['orgRef']['@handle'])
-
-            for cloud_provider in cloud_providers:
-                if bool(re.search(cloud_provider, org_name.lower())):
-
-                    cloud_based = cloud_providers.index(cloud_provider) + 1
-                    break
-            
         row.append(cloud_based)
-    except:
-        pass
+        row.append(org_name)
+        row.append(org_handle)
+        rows.append(row)
+    
+        if not rank % 100:
+            print "indexed %s" % rank   
+    
+        rank += 1
+        time.sleep(1)
+        print row
+    
+    
+    with open('top_2000.csv', 'w') as fp:
+        a = csv.writer(fp, delimiter=',')
+        a.writerows(rows)
+        
+        
+def convert_to_table():
+    """
+    Read rank, name, address, etc data from csv and write it out to 
+    an HTML table
+    """
+    
+    print "converting to table"
+    
+    with open('top_2000.csv', 'rb') as csvfile:
+        rows = csv.reader(csvfile, delimiter=',')
+        for row in rows:
+            print "<tr>"
+            for item in row:
+                print "<td>%s</td>" % item
+            print "</tr>"
 
-    rows.append(row)
+def get_aggregate_table():
+    """
+    Get aggregate numbers
+    """
     
-    if not rank % 100:
-        print "indexed %s" % rank   
-    
-    rank += 1
-    time.sleep(1)
-    
-with open('top_2000.csv', 'w') as fp:
-    a = csv.writer(fp, delimiter=',')
-    a.writerows(rows)
+    with open('top_2000.csv', 'rb') as csvfile:
+        rows = csv.reader(csvfile, delimiter=',')
+        for row in rows:
+            print row
+
+if __name__ == "__main__":
+    get_arin_data()
+    #convert_to_table()
