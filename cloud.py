44aa4a8fc696e813c0c42bf78bb637e75410fa20
Matthew Phillips <matt@Rhondas-iMac.local>
2014-01-05 16:33:17 -0500

    a little organization. added some initital results.

--- cloud.py
+++ cloud.py
@@ -1,6 +1,7 @@
 import dns.resolver
-import time, json, re, csv
+import time, json, re, csv, ConfigParser, re
 import requests
+import tldextract
 
 # loop through lines in top_2000_alexa_sites.txt
 # for line, get the IP of the name contained in the line
@@ -14,15 +15,27 @@ import requests
 
 
 
-def get_arin_data():
+def get_arin_data(domain_list, result_file):
+    """
+    Read in file containing contain containing domain names and using the ARIN
+    API, get the owner of the IP. If the owner matches a string in our list of
+    cloud owners, mark it
+    
+    domain_list = file containing list of domains. We'll get details from ARIN
+    for each domain in this list
+    
+    result_file = file we'll write the details we get from ARIN, for each 
+    domain
+    """
+
+    print "gettings arin data"
 
     # Some common cloud hosts
     cloud_providers = ['amazon', 'rackspace', 'softlayer', 'microsoft', 'google']
 
-
     rows = []
 
-    f = open('top_2000_alexa_sites.txt', 'r')
+    f = open(domain_list, 'r')
 
     rank = 1
 
@@ -30,6 +43,11 @@ def get_arin_data():
         row = []
         row.append(rank)
         name = line.rstrip()
+        name_parts = tldextract.extract(name)
+        name = name_parts.domain + '.' + name_parts.suffix
+        org_name = ""
+        org_handle = ""
+        cloud_based = 0
     
         try:
             answers = dns.resolver.query(name, 'A')
@@ -39,11 +57,7 @@ def get_arin_data():
             url = 'http://whois.arin.net/rest/ip/' + ip_address
             headers = {'Accept': 'application/json'}
             r = requests.get(url, headers=headers)
-            jr = json.loads(r.text)
-        
-            org_name = ""
-            org_handle = ""
-            cloud_based = 0
+            jr = json.loads(r.text)    
 
             if 'net' in jr and 'orgRef' in jr['net'] and '@name' in jr['net']['orgRef']:
                 org_name = jr['net']['orgRef']['@name']
@@ -68,23 +82,115 @@ def get_arin_data():
         if not rank % 100:
             print "indexed %s" % rank
     
-        rank += 1
-        time.sleep(1)
+        #print row
     
-    with open('top_2000.csv', 'w') as fp:
+        rank += 1
+        time.sleep(.5)
+        
+    
+    with open(result_file, 'w') as fp:
         a = csv.writer(fp, delimiter=',')
         a.writerows(rows)
         
+def get_crunchbase_data(crunchbase_key, raw_companies_file):
+    """
+    given a year, get the domain names for all startups using the cruncbase api
+    """
+    
+    # load in all crunchbase companies (many MBs)
+    json_data = open(raw_companies_file)
+    companies = json.load(json_data)
+    json_data.close()
+    
+    print "processing %s companies" % len(companies)
+    
+    count = 1
+    
+    for company in companies[197700:]:
         
-def convert_to_table():
+        payload = {'api_key': crunchbase_key}
+    
+        url = 'http://api.crunchbase.com/v/1/company/' + company['permalink'] + '.js'
+        r = requests.get(url, params=payload)
+        r_text = r.text
+        # somteimes we don't get a json object
+        if not r_text:
+            next
+        
+        try:
+            jr = json.loads(r_text.replace('\r\n', ''))
+        except:
+            print "cloudn't load %s " % r.url
+            next
+        
+        #print json.dumps(jr, sort_keys=True, indent=4, separators=(',', ': '))
+        
+        
+        if 'deadpooled_url' in jr and 'deadpooled_year' in jr and 'total_money_raised' in jr:
+            if jr['deadpooled_url'] or jr['deadpooled_year'] or not jr['total_money_raised']:
+                next
+
+        try:
+            money_raised = re.sub(r"\D", "", jr['total_money_raised'])
+        
+            if float(money_raised) <= 0:
+                next
+        except:
+            next
+        
+        if 'founded_year' in jr and 'homepage_url' in jr and jr['homepage_url']:
+
+            # todo: wrap this up in a function
+            if jr['founded_year'] == 2006:
+                with open("data/crunchbase_details_2006.csv", "a") as myfile:
+                    myfile.write(jr['homepage_url'] + '\n')
+
+            if jr['founded_year'] == 2007:
+                with open("data/crunchbase_details_2007.csv", "a") as myfile:
+                    myfile.write(jr['homepage_url'] + '\n')
+                
+            if jr['founded_year'] == 2008:
+                with open("data/crunchbase_details_2008.csv", "a") as myfile:
+                    myfile.write(jr['homepage_url'] + '\n')
+
+            if jr['founded_year'] == 2009:
+                with open("data/crunchbase_details_2009.csv", "a") as myfile:
+                    myfile.write(jr['homepage_url'] + '\n')
+                
+            if jr['founded_year'] == 2010:
+                with open("data/crunchbase_details_2010.csv", "a") as myfile:
+                    myfile.write(jr['homepage_url'] + '\n')
+
+            if jr['founded_year'] == 2011:
+                with open("data/crunchbase_details_2011.csv", "a") as myfile:
+                    myfile.write(jr['homepage_url'] + '\n')
+                
+            if jr['founded_year'] == 2012:
+                with open("data/crunchbase_details_2012.csv", "a") as myfile:
+                    myfile.write(jr['homepage_url'] + '\n')
+
+            if jr['founded_year'] == 2013:
+                with open("data/crunchbase_details_2013.csv", "a") as myfile:
+                    myfile.write(jr['homepage_url'] + '\n')
+
+        if not count % 100:
+            print "processed %s " % count
+
+
+        count += 1
+
+        
+def convert_to_table(result_file):
     """
     Read rank, name, address, etc data from csv and write it out to 
     an HTML table
+    
+    result_file = file with ARIN details. 
     """
     
     print "converting to table"
     
-    with open('top_2000.csv', 'rb') as csvfile:
+    with open(result_file, 'rb') as csvfile:
         rows = csv.reader(csvfile, delimiter=',')
         for row in rows:
             print "<tr>"
@@ -92,16 +198,45 @@ def convert_to_table():
                 print "<td>%s</td>" % item
             print "</tr>"
 
-def get_aggregate_table():
+def get_aggregate_table(result_file, cloud_providers):
     """
-    Get aggregate numbers
+    Get aggregate numbers. Count up names in cloud. Get percent of names and
+    dump them out in order.
     """
+    print "Aggregating results"
     
-    with open('top_2000.csv', 'rb') as csvfile:
+    # Some common cloud hosts
+    cloud_providers = ['amazon', 'rackspace', 'softlayer', 'microsoft', 'google']
+    
+    with open(result_file, 'rb') as csvfile:
         rows = csv.reader(csvfile, delimiter=',')
         for row in rows:
-            print row
+            if row[3] > 0:
+                print row
 
 if __name__ == "__main__":
-    get_arin_data()
-    #convert_to_table()
+    """
+    get alexa names, get the top 2000 sites from alex and dump them in a file
+    
+    get arin data, given a csv file of domain names, create a csv file
+    of name, ip, ownership, cloud hosted, and other detals
+    
+    get crunchbase data, get the domain names of startups created in the last year
+    
+    create html table of names, given a csv of arin data, create an html table
+    
+    create html table of aggregate, given a csv file of arin data, aggregate cloud
+    usage and print html table
+    
+    """
+    
+    # Config file for things we don't want git to track
+    config = ConfigParser.RawConfigParser()
+    config.read('etc/settings.cfg')
+    crunchbase_key = config.get('general', 'crunchbase_key')
+    
+    
+    #get_crunchbase_data(crunchbase_key, 'crunchbase_companies.json')
+    get_arin_data('data/crunchbase_details_2010.txt', 'data/results/crunchbase_2010.csv')
+    #convert_to_table('top_2000.csv')
+    #get_aggregate_table('top_2000.csv')
